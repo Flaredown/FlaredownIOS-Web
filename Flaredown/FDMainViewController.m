e5db1312d99dde0d81f8332a6ce830c8b491a9a4
Vitalii Komisarov <udrzets@gmail.com>
2017-01-23 19:03:11 +0200

    No internet connection alert

--- Flaredown/FDMainViewController.m
+++ Flaredown/FDMainViewController.m
@@ -12,6 +12,8 @@
 
 @interface FDMainViewController ()
 
+@property (nonatomic, strong) UIView *noInternetView;
+
 @end
 
 @implementation FDMainViewController
@@ -47,5 +49,45 @@
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     [_webView loadRequest:request];
 }
+
+- (void)reload {
+    
+    [self.noInternetView removeFromSuperview];
+    [self.webView reload];
+}
+
+#pragma mark -
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+    
+    self.noInternetView = [[UIView alloc] initWithFrame:self.view.bounds];
+    self.noInternetView.backgroundColor = [UIColor colorWithRed:0.94 green:0.92 blue:0.89 alpha:1.00];
+    [self.view addSubview:self.noInternetView];
+    
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, self.noInternetView.frame.size.height / 2 - 70, self.noInternetView.frame.size.width, 30)];
+    titleLabel.textColor = [UIColor colorWithRed:0.73 green:0.66 blue:0.55 alpha:1.00];
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    titleLabel.font = [UIFont boldSystemFontOfSize:24];
+    titleLabel.text = @"Connection not found :(";
+    [self.noInternetView addSubview:titleLabel];
+    
+    UILabel *messageLabel = [[UILabel alloc] initWithFrame:CGRectMake(80, self.noInternetView.frame.size.height / 2 - 70, self.noInternetView.frame.size.width - 160, 100)];
+    messageLabel.textColor = [UIColor colorWithRed:0.73 green:0.66 blue:0.55 alpha:1.00];
+    messageLabel.textAlignment = NSTextAlignmentCenter;
+    messageLabel.font = [UIFont boldSystemFontOfSize:14];
+    messageLabel.numberOfLines = 2;
+    messageLabel.text = @"Make sure you're connected to the internet and try again";
+    [self.noInternetView addSubview:messageLabel];
+    
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 200, 40)];
+    button.center = CGPointMake(self.noInternetView.frame.size.width / 2, self.noInternetView.frame.size.height / 2 + 30);
+    [button addTarget:self action:@selector(reload) forControlEvents:UIControlEventTouchUpInside];
+    [button setTitle:@"Try again" forState:UIControlStateNormal];
+    button.backgroundColor = [UIColor colorWithRed:0.38 green:0.72 blue:0.69 alpha:1.00];
+    button.layer.cornerRadius = 5;
+    [self.noInternetView addSubview:button];
+    
+}
+
 
 @end
